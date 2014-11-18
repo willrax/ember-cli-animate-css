99d03df53620aa548bb118056250a57679705f53
Will Raxworthy <me@willrax.com>
2014-11-18 19:18:50 +1100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  name: "ember-cli-pure",
+
+  included: function included(app) {
+    this.app = app;
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + "/animate.css/animate.min.css");
+  }
+};
